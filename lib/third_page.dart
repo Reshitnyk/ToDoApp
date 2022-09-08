8e00458300c8063dd93f255ee30b6a18bffc42fe
Reshitnyk <113026371+Reshitnyk@users.noreply.github.com>
2022-09-08 11:11:16 +0300

    Create third_page.dart

---
+++ lib/third_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ThirdScreen extends StatefulWidget {
+  const ThirdScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ThirdScreen> createState() => _ThirdScreenState();
+}
+
+class _ThirdScreenState extends State<ThirdScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
